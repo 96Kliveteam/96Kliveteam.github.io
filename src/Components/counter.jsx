b06f9444a2812b7b7ba8a9013b9e689d94fe3efb
tam.hoang@smsdome.com <tam.hoang@smsdome.com>
2024-06-03 20:08:11 +0700

    updated

---
+++ src/Components/counter.jsx
@@ -0,0 +1,42 @@
+import { tab } from '@testing-library/user-event/dist/tab';
+import React, { Component, useState } from 'react';
+
+function Counter({cities, onSelectedChange}) {
+    const [state, setState] = useState({
+        count: 0,
+    });
+    const [selectedIndex, setSelectedIndex] = useState(-1)
+    const style = {
+        fontSize: 50
+    }
+     
+    const GetBadgeClassesMethod=()=> {
+        let classes = "badge m-2 text-bg-";
+        classes += state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+    
+    const formatCount = ()=> {
+        const { count } = state
+        return count === 0 ? <h1>Zero</h1> : count;
+    }
+    const renderTags = ()=> {
+        if (cities.name.length === 0) return (<p>There is no tags!</p>)
+
+        return (
+            <ul className='list-group'>
+                {cities.name.map((tab, index) => <li key={tab} className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} onClick={() => setSelectedIndex(index)}>{tab}</li>)}
+            </ul>
+        )
+    }
+        return (
+            <>
+                <span className={GetBadgeClassesMethod()} style={{fontSize: style.fontSize}}>{formatCount()}</span>
+                <button className='btn btn-primary btn-sm'>Increment</button>
+                {cities.name.length === 0 && <h1>Please create a new tag!</h1>}
+                {renderTags()}
+            </>
+        );
+}
+
+export default Counter
